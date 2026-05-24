58213c82b9dd898fc0f7f976990dc1142727ad35
Daniel Niu <daniel.n@wustl.edu>
2026-05-24 12:07:16 +0800

    updated BSP

--- camera_platform/ps7_cortexa9_0/standalone_ps7_cortexa9_0/bsp/libsrc/build_configs/gen_bsp/CMakeFiles/clean_additional.cmake
+++ camera_platform/ps7_cortexa9_0/standalone_ps7_cortexa9_0/bsp/libsrc/build_configs/gen_bsp/CMakeFiles/clean_additional.cmake
@@ -3,9 +3,15 @@ cmake_minimum_required(VERSION 3.16)
 
 if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
   file(REMOVE_RECURSE
+  "C:\\Users\\niumu\\Refocusing\\refocus_cam_v2\\camera_platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\include\\diskio.h"
+  "C:\\Users\\niumu\\Refocusing\\refocus_cam_v2\\camera_platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\include\\ff.h"
+  "C:\\Users\\niumu\\Refocusing\\refocus_cam_v2\\camera_platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\include\\ffconf.h"
   "C:\\Users\\niumu\\Refocusing\\refocus_cam_v2\\camera_platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\include\\sleep.h"
+  "C:\\Users\\niumu\\Refocusing\\refocus_cam_v2\\camera_platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\include\\xilffs.h"
+  "C:\\Users\\niumu\\Refocusing\\refocus_cam_v2\\camera_platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\include\\xilffs_config.h"
   "C:\\Users\\niumu\\Refocusing\\refocus_cam_v2\\camera_platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\include\\xiltimer.h"
   "C:\\Users\\niumu\\Refocusing\\refocus_cam_v2\\camera_platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\include\\xtimer_config.h"
+  "C:\\Users\\niumu\\Refocusing\\refocus_cam_v2\\camera_platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\lib\\libxilffs.a"
   "C:\\Users\\niumu\\Refocusing\\refocus_cam_v2\\camera_platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\lib\\libxiltimer.a"
   )
 endif()
